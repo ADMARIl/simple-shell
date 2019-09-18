784a98364491f7abe8cac8eb8f17f9e0b0ac5898
Andrew <aings1@umbc.edu>
2019-09-18 19:32:02 -0400

    Created basic framework for simple-shell file

---
+++ simple_shell.c
@@ -0,0 +1,18 @@
+/*
+  File: simple_shell.h
+  Author: Andrew Ingson (aings1@umbc.edu)
+  Date: 9/18/2019
+  Project: CMSC 421 (Principles of Operating Sytems) HW 1
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/syscall.h>
+#include <errno.h>
+
+int main() {
+
+
+  return 0;
+}
